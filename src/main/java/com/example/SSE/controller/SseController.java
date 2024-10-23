91d07417ab4bca0caef01d6b90463d190732a3e8
LilianMar <lilian.maradiago@gmail.com>
2024-10-22 20:07:44 -0500

    proyecto SSE

---
+++ src/main/java/com/example/SSE/controller/SseController.java
@@ -0,0 +1,17 @@
+package com.example.SSE.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+public class SseController {
+
+    @GetMapping(path = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter suscribe() {
+    SseEmitter emitter= new SseEmitter(Long.MAX_VALUE);
+    return emitter;
+    }
+
+}
